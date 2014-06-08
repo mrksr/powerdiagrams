d4e0981d60a3ac1d9dbf1611b51673f1cd7081f3
Markus Kaiser <markus.kaiser@in.tum.de>
2014-06-09 01:49:25 +0200

    Call Vertices Values; Add accessors for both directions

--- include/IncidenceLattice.h
+++ include/IncidenceLattice.h
@@ -4,40 +4,74 @@
 #include "BidirectionalGraph.h"
 #include <map>
 
-template <typename Vertex_t>
+template <typename Value_t>
 class IncidenceLattice {
     typedef int Key_t;
+    typedef typename BidirectionalGraph<Key_t, Value_t>::Keys_t Keys_t;
 
     public:
-        IncidenceLattice(const Vertex_t& defaultVertex):
+        IncidenceLattice(const Value_t& defaultValue):
             rep_(),
             keys_(),
             nextKey_(0),
-            defaultVertex_(defaultVertex)
+            defaultValue_(defaultValue)
         { }
         virtual ~IncidenceLattice() { }
 
-        Key_t keyOf(const Vertex_t& vertex)
+        Key_t key(const Value_t& value)
         {
-            return keys_[vertex];
+            return keys_[value];
+        }
+        Value_t value(const Key_t& key)
+        {
+            return rep_.value(key);
         }
 
-        Key_t addVertex(const Vertex_t& vertex)
+        Keys_t minimals()
+        {
+            return rep_.minimalElements();
+        }
+        Keys_t maximals()
+        {
+            return rep_.maximalElements();
+        }
+
+        Keys_t minimalsOf(const Key_t& key)
+        {
+            return rep_.minimalPredecessors(key);
+        }
+        Keys_t maximalsOf(const Key_t& key)
+        {
+            return rep_.maximalSuccessors(key);
+        }
+
+        Key_t addMinimal(const Value_t& value)
         {
             auto key = nextKey();
-            rep_.insertNode(key, vertex);
-            keys_[vertex] = key;
+            rep_.insertNode(key, value);
+            keys_[value] = key;
 
             return key;
         }
+        Key_t addFace(const Keys_t& face)
+        {
+            auto key = nextKey();
+            rep_.insertNode(key, defaultValue_);
+
+            for (auto& item : face) {
+                rep_.insertEdge(item, key);
+            }
+        }
 
     private:
-        BidirectionalGraph<Key_t, Vertex_t> rep_;
-        std::map<Vertex_t, Key_t> keys_;
+        BidirectionalGraph<Key_t, Value_t> rep_;
+        std::map<Value_t, Key_t> keys_;
         Key_t nextKey_;
-        Vertex_t defaultVertex_;
+        Value_t defaultValue_;
 
-        Key_t nextKey() { return nextKey_++; }
+        Key_t nextKey() {
+            return nextKey_++;
+        }
 };
 
 #endif
