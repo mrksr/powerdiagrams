c7c25174a3b48bcd660cb49411bc4c83dabf9fb9
Markus Kaiser <markus.kaiser@in.tum.de>
2014-06-22 17:23:04 +0200

    remove debug output, add convex hull algorithm

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,4 @@
+#include "ConvexHullQhull.h"
 #include "PowerDiagram.h"
 #include <Eigen/Dense>
 #include <iostream>
@@ -8,33 +9,6 @@
 int main(int argc, char *argv[])
 {
     {
-        std::cout << "Some Graph Tests:" << std::endl;
-        BidirectionalGraph<std::string, int> graph;
-
-        graph.insertNode("wurst", 5);
-        graph.insertNode("salat", 10);
-        graph.insertNode("soße", 15);
-        graph.insertEdge("wurst", "salat");
-        graph.insertEdge("salat", "soße");
-        auto succs = graph.allSuccessors("wurst");
-        for (auto& item : succs) {
-            std::cout << "Successor: " << item << std::endl;
-        }
-        for (auto& item : graph.allPredecessors("soße")) {
-            std::cout << "Predecessor: " << item << std::endl;
-        }
-
-        for (auto& item : graph.minimalElements()) {
-            std::cout << "Minimal: " << item << std::endl;
-        }
-        for (auto& item : graph.maximalElements()) {
-            std::cout << "Maximal: " << item << std::endl;
-        }
-    }
-
-    std::cout << std::endl << std::endl;
-
-    {
         std::vector<PowerDiagram::Sphere_t> spheres {
             PowerDiagram::sphere(Eigen::Vector3d(1, 0, 0), 2),
             PowerDiagram::sphere(Eigen::Vector3d(0, 0, 1), 1),
@@ -42,7 +16,20 @@ int main(int argc, char *argv[])
             PowerDiagram::sphere(Eigen::Vector3d(0, 0, 2), 1),
             PowerDiagram::sphere(Eigen::Vector3d(2, 0, 0), 1)
         };
-        PowerDiagram::fromSpheres(spheres);
+
+        std::cout << "Spheres:" << std::endl;
+        for (auto& item : spheres) {
+            std::cout
+                << "Center: "
+                << std::get<0>(item).transpose()
+                << " - Radius: "
+                << std::get<1>(item)
+                << std::endl;
+        }
+        std::cout << std::endl << std::endl;
+
+        ConvexHullQhull conv;
+        PowerDiagram::fromSpheres(conv, spheres);
     }
     return 0;
 }
